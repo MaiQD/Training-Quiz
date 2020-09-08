fbe265e7c3ec748984ea07a63a8851d2f24f7d78
MaiQD <q.dat2710@gmail.com>
2020-09-09 00:01:05 +0700

    phải cài thêm thư viện mới có ToTable

---
+++ Training-Quiz.API/TrainingQuiz.Data/Mapping/QG/QuizTypeMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TrainingQuiz.Core.Domain.QG;
+
+namespace TrainingQuiz.Data.Mapping.QG
+{
+	public partial class QuizTypeMap: MaiQDEntityTypeConfiguration<QuizType>
+	{
+		public override void Configure(EntityTypeBuilder<QuizType> builder)
+		{
+			builder.ToTable("QG_QuizType");
+			builder.HasKey(p => p.ID);
+			base.Configure(builder);
+		}
+	}
+}
